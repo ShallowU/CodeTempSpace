b5883b9ab7fb9da3a895bf4926997a572a18d3e6
Dianfu Zhou <130028165+ShallowU@users.noreply.github.com>
2024-02-27 10:48:53 +0000

    secondtest

--- code1/1.cpp
+++ code1/1.cpp
@@ -1,8 +1,29 @@
 #include<iostream>
+#include<cmath>
+#include<cstdio>
+#include<cstring>
+#include<cstdlib>
+#include<algorithm>
+#include<map>
+#include<set>
+#include<queue>
+#include<stack>
+#include<list>
+#include<vector>
+#include<deque>
+#include<bitset>
+#include<functional>
 using namespace std;    
 int main()
 {
+    
+
+    cout<<3+3<<endl;
     cout<<"Hello World";
+
+    cout << "hello world" << endl;
+    cout << "hello world" << endl;
+    cout << "hello world" << endl;
     cout << "hello shallowu" << endl;
     return 0;
 }
